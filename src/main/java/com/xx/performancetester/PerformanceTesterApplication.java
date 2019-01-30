9354536dd2dedb974a744fa98f9e59a29c30125a
jennifer <313882613@qq.com>
2019-01-30 22:13:24 +0800

    first

---
+++ src/main/java/com/xx/performancetester/PerformanceTesterApplication.java
@@ -0,0 +1,14 @@
+package com.xx.performancetester;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerformanceTesterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PerformanceTesterApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/xx/performancetester/PerformanceTesterApplicationTests.java
@@ -0,0 +1,17 @@
+package com.xx.performancetester;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PerformanceTesterApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
